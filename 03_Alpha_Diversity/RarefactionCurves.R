632d278b49dba830cfb5de6fd1585d92d4065af4
Susanne Walden <s.walden@uni-koeln.de>
2021-02-23 15:46:57 +0100

    added alpha diversity code

--- 03_Alpha_Diversity/RarefactionCurves.R
+++ 03_Alpha_Diversity/RarefactionCurves.R
@@ -1,105 +1,124 @@
-
-
-##########################################################################################################################################
-##########################################################################################################################################
-######################################################### Seasonnal Effect ###############################################################
-##########################################################################################################################################
-##########################################################################################################################################
-
-
 rm(list = ls())
 
+library(iNEXT)
 library(ggplot2)
-library(vegan)
 library(RColorBrewer)
-library(reshape2)#for melting
-library(agricolae)
-
-#Seasonal Susi Cerco + Endo
-
-setwd("C:/Users/Susi/Desktop/season_server/")
-
-
-OTU_Table = as.data.frame(read.csv2("AllSeason_Cerc_En_Final_OTU_R_7633_edit_plottogether_extended.csv",header = T))
-
-SampleMetadata = as.data.frame(OTU_Table[,1:17])
-OTU_Table = OTU_Table[,18:ncol(OTU_Table)]
+library(ggpubr)
 
 
 
-############ Calculate Alpha Diversity Indices
-richness = specnumber(OTU_Table)
-df = as.data.frame(richness)
 
+#AllSeason
+setwd("C:/Users/Susi/Desktop/season_server/Functional_Traits/F19/")
+OTU_Table = as.data.frame(read.csv2("AllSeason_F19_Cerc_En_Final_OTU_R_7633.csv",header = T))
 
-df$simpson = diversity(OTU_Table, index = "simpson")
-df$shannon = diversity(OTU_Table, index = "shannon")
-df$eveness = df$shannon/log(df$richness)
+SampleMetadata = OTU_Table[,1:5]
+rownames(OTU_Table) = SampleMetadata$SampleID
+rownames(SampleMetadata) = SampleMetadata$SampleID
+species = OTU_Table[,6:ncol(OTU_Table)]
+species_mat = as.matrix(species)
 
-
-rownames(df) = SampleMetadata$ï..SampleID # watch out: sometimes R adds "ï.." to the titel of the first column, probably due to .csv
-
-############################## extend table with metadata
-
-df$Microhabitat = SampleMetadata$Microhabitat
-df$TreeSpecies = SampleMetadata$TreeSpecies
-df$Season = SampleMetadata$Season
-df$SamplingDescription = SampleMetadata$SamplingDescription
-df$MicrohabitatPerSampling = SampleMetadata$MicrohabitatPerSampling
-df$SamplingDescription = factor(df$SamplingDescription, levels = c('Autumn 2017','Spring 2018','Autumn 2018', 'Spring 2019' ))
-
-
-######## melt table so you end up with only one row of alpha div values
-
-df_melted = melt(df)
+#We want to test the rarefaction for the microhabitats, so now we aggregate the OTU table
+species_mat_aggregated = aggregate(species_mat, by = list(SampleMetadata$Microhabitat), FUN = "sum")
+rownames(species_mat_aggregated) = levels(list(SampleMetadata$Microhabitat)[[1]])
+species_mat_aggregated = species_mat_aggregated[,-1]
 
 
 
-################################
 
-g = ggplot(df_melted, aes(x = Microhabitat, y = value, fill = Microhabitat)) + 
-  stat_boxplot(geom = "errorbar", width = 0.1, show.legend = F) +
-  geom_boxplot(show.legend = F) + 
-  scale_fill_manual(values = c("#138F6A","#FF944D","#FFE4B5","#BF6692","#A8ACFF","#4363d8","#8B8989","#9A6324","#800000"), 
+
+#H17
+Cercozoa_out_A = iNEXT(t(species_mat_aggregated), q = 0, #q = 0 means species richness
+    datatype = "abundance", nboot = 99, conf = 0.97, knots = 250, endpoint = NULL)
+save(Cercozoa_out_A, file = "Cercozoa_out_A.RData")
+#load("Cercozoa_out_A.RData")
+#F18
+Cercozoa_out_B = iNEXT(t(species_mat_aggregated), q = 0, #q = 0 means species richness
+                     datatype = "abundance", nboot = 99, conf = 0.97, knots = 250, endpoint = NULL)
+save(Cercozoa_out_B, file = "Cercozoa_out_B.RData")
+#load("Cercozoa_out_B.RData")
+#H18
+Cercozoa_out_C = iNEXT(t(species_mat_aggregated), q = 0, #q = 0 means species richness
+                       datatype = "abundance", nboot = 99, conf = 0.97, knots = 250, endpoint = NULL)
+save(Cercozoa_out_C, file = "Cercozoa_out_C.RData")
+#load("Cercozoa_out_C.RData")
+#F19
+Cercozoa_out_D = iNEXT(t(species_mat_aggregated), q = 0, #q = 0 means species richness
+                       datatype = "abundance", nboot = 99, conf = 0.97, knots = 250, endpoint = NULL)
+save(Cercozoa_out_D, file = "Cercozoa_out_D.RData")
+#load("Cercozoa_out_D.RData")
+
+#Cercozoa_out_A
+#Cercozoa_out_B
+#Cercozoa_out_C
+#Cercozoa_out_D
+
+
+
+################### Plot
+
+
+
+g = ggiNEXT(Cercozoa_out_D, type = 1, color.var = "site") +
+  scale_fill_manual(name = "Microhabitat", 
+                    values = c("#006d2c","#fd8d3c","#fed976","#d4b9da","#045a8d","#0570b0","#a6bddb","#8e8878","#524640"), 
                     limits = c("Fresh Leaves","Bark","Arboreal Soil", "Deadwood","Hypnum","Orthotrichum","Lichen","Leaf Litter", "Soil")) + 
-  scale_x_discrete(limits = c("Fresh Leaves","Deadwood","Lichen","Bark","Orthotrichum","Hypnum","Arboreal Soil","Leaf Litter", "Soil")) + 
-  #scale_fill_manual(values = c("#138F6A","#FF944D","#FFE4B5","#BF6692","#A8ACFF","#4363d8","#8B8989","#9A6324","#800000"), 
-   #                 limits = c("Fresh Leaves","Bark","Arboreal Soil", "Deadwood","Hypnum","Orthotrichum","Lichen","Leaf Litter", "Soil")) + 
-  #scale_x_discrete(limits = c("Fresh Leaves","Bark","Arboreal Soil", "Deadwood","Hypnum","Orthotrichum","Lichen","Leaf Litter", "Soil")) +  
-  #scale_y_continuous(limits = c(0, 1)) +
-  theme_minimal() + 
-  labs(y = "Alpha Diversity", 
-       x = "Microhabitat")+ 
-      
-  theme(axis.text=element_text(size=14, face = "bold"), 
-        axis.title=element_text(size=16, face = "bold"), 
-        #plot.title = element_text(size = 14, face = "bold", hjust = 0.5), 
-        plot.subtitle = element_text(size = 16, hjust = 0.5), 
-        #legend.text = element_text(size = 10), 
-       # legend.title = element_text(size = 12, face = "bold"),
-        strip.text = element_text(size = 16, face = "bold"),
-        axis.text.x = element_text(angle=45, hjust = 1))+
- facet_grid(variable ~ SamplingDescription, scales = "free", switch = "y")+
- theme(plot.margin = margin(1,1,1,1, "cm")) 
+  scale_color_manual(name = "Microhabitat", 
+                     values = c("#006d2c","#fd8d3c","#fed976","#d4b9da","#045a8d","#0570b0","#a6bddb","#8e8878","#524640"), 
+                     limits = c("Fresh Leaves","Bark","Arboreal Soil", "Deadwood","Hypnum","Orthotrichum","Lichen","Leaf Litter", "Soil")) + 
+  scale_shape_manual(name = "Microhabitat", 
+                     values = rep(16, 9), 
+                     limits = c("Fresh Leaves","Bark","Arboreal Soil", "Deadwood","Hypnum","Orthotrichum","Lichen","Leaf Litter", "Soil")) + 
+  theme_minimal() +
+  labs(title = "Spring 2019", 
+       x = "Number of sequences", y = "Number of OTUs") +
+  scale_x_continuous(labels = scales::comma) +
+  theme(legend.text = element_text(size = 16), 
+        legend.title = element_text(size = 16, face = "bold"), 
+        legend.position = "right",
+        legend.direction = "vertical", 
+        plot.title = element_text(size = 16, face = "bold", hjust = 0.5, vjust = 0.5), 
+        legend.title.align = 0.5,
+        axis.title = element_text(size = 16, face = "bold"), 
+        axis.text = element_text(size = 14)) +
+  guides(fill = guide_legend(title="Microhabitat"), 
+         color = guide_legend(title="Microhabitat"), 
+         shape = guide_legend(title="Microhabitat"))
+
+# In the default plot, brighter colors are hard to see.
+# So here we rearrange the layers to make them more visible
+g$layers = c(g$layers[[3]], g$layers[[2]], g$layers[[1]])
 
 g
-#write.csv(df_melted, file = "AlphaDiv_Values.csv")
 
-###### export 
-#write.csv(df_melted, file = "Melted_AlphaDiv.csv")
-#write.csv(df, file = "Unmelted_AlphaDiv.csv")
+A=g
+B=g
+C=g
+D=g
 
-#save
 
-ggsave("AlphaDiv_300_336x252.jpeg", plot = g, 
+############plot together 
+combi = ggarrange( A,B,C,D,
+                   labels = c("A", "B","C","D"), 
+                   ncol = 2, nrow = 2, font.label = list(size = 16, color = "black"), 
+                   common.legend = T, legend = "right", 
+                   align = "v")+
+  theme(plot.margin = margin(1,1,1,1, "cm")) 
+
+combi
+
+#############save
+ggsave("Rarefaction_300_336x252.jpeg", plot = combi, 
        device = "jpeg", dpi = 300, width = 336, height = 252, 
        units = "mm")
-ggsave("AlphaDiv_600_336x252.pdf", plot = g, 
+ggsave("Rarefaction_600_336x252.pdf", plot = combi, 
        device = "pdf", dpi = 600, width = 336, height = 252, 
        units = "mm")
-ggsave("AlphaDiv_600_336x252.tif", plot = g, 
+ggsave("Rarefaction_600_336x252.tif", plot = combi, 
        device = "tiff", dpi = 600, width = 336, height = 252, 
        units = "mm")
-ggsave("AlphaDiv_600_336x252.png", plot = g, 
+ggsave("Rarefaction_600_336x252.png", plot = combi, 
        device = "png", dpi = 600, width = 336, height = 252, 
        units = "mm")
+
+
